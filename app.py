3dbec0d900318dc751dd64b41fe88b2035465322
Arek <arekb86@gmail.com>
2025-10-19 22:06:42 +0200

    Style tables to match dark theme

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ if not _secret:
         "SECRET_KEY environment variable not set; using a temporary key for this run."
     )
 app.config['SECRET_KEY'] = _secret
+app.config['ASSET_VERSION'] = os.environ.get('ASSET_VERSION', '2')
 
 # Register Jinja custom filters
 app.jinja_env.filters['euro_datetime'] = euro_datetime
@@ -28,7 +29,7 @@ app.jinja_env.filters['euro_datetime'] = euro_datetime
 
 @app.context_processor
 def inject_globals():
-    return {"current_year": datetime.now().year}
+    return {"current_year": datetime.now().year, "asset_version": app.config.get('ASSET_VERSION', '1')}
 
 
 # Register blueprints
